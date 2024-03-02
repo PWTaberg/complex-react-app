fdf423ce0ff76c854e621e5de3115b9383c14a86
Peter <pwelliver@yahoo.com>
2024-03-02 18:54:35 +0100

    chat feature added

--- app/Main.js
+++ app/Main.js
@@ -1,28 +1,29 @@
-import React, { useState, useReducer, useEffect } from "react"
-import ReactDOM from "react-dom/client"
-import { useImmerReducer } from "use-immer"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { CSSTransition } from "react-transition-group"
-import Axios from "axios"
-Axios.defaults.baseURL = "http://localhost:8080"
+import React, { useState, useReducer, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import { useImmerReducer } from "use-immer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
+import Axios from "axios";
+Axios.defaults.baseURL = "http://localhost:8080";
 
-import StateContext from "./StateContext"
-import DispatchContext from "./DispatchContext"
+import StateContext from "./StateContext";
+import DispatchContext from "./DispatchContext";
 
 // My Components
-import Header from "./components/Header"
-import HomeGuest from "./components/HomeGuest"
-import Home from "./components/Home"
-import Footer from "./components/Footer"
-import About from "./components/About"
-import Terms from "./components/Terms"
-import CreatePost from "./components/CreatePost"
-import ViewSinglePost from "./components/ViewSinglePost"
-import FlashMessages from "./components/FlashMessages"
-import Profile from "./components/Profile"
-import EditPost from "./components/EditPost"
-import NotFound from "./components/NotFound"
-import Search from "./components/Search"
+import Header from "./components/Header";
+import HomeGuest from "./components/HomeGuest";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Terms from "./components/Terms";
+import CreatePost from "./components/CreatePost";
+import ViewSinglePost from "./components/ViewSinglePost";
+import FlashMessages from "./components/FlashMessages";
+import Profile from "./components/Profile";
+import EditPost from "./components/EditPost";
+import NotFound from "./components/NotFound";
+import Search from "./components/Search";
+import Chat from "./components/Chat";
 
 function Main() {
   const initialState = {
@@ -33,43 +34,59 @@ function Main() {
       username: localStorage.getItem("complexappUsername"),
       avatar: localStorage.getItem("complexappAvatar")
     },
-    isSearchOpen: false
-  }
+    isSearchOpen: false,
+    isChatOpen: false,
+    unreadChatCount: 0
+  };
 
   function ourReducer(draft, action) {
     switch (action.type) {
       case "login":
-        draft.loggedIn = true
-        draft.user = action.data
-        return
+        draft.loggedIn = true;
+        draft.user = action.data;
+        return;
       case "logout":
-        draft.loggedIn = false
-        return
+        draft.loggedIn = false;
+        return;
       case "flashMessage":
-        draft.flashMessages.push(action.value)
-        return
+        draft.flashMessages.push(action.value);
+        return;
       case "openSearch":
-        draft.isSearchOpen = true
-        return
+        draft.isSearchOpen = true;
+        return;
       case "closeSearch":
-        draft.isSearchOpen = false
-        return
+        draft.isSearchOpen = false;
+        return;
+      case "toggleChat":
+        draft.isChatOpen = !draft.isChatOpen;
+        return;
+      case "closeChat":
+        draft.isChatOpen = false;
+        return;
+
+      case "incrementUnreadChatCount":
+        draft.unreadChatCount++;
+        return;
+
+      case "clearUnreadChatCount":
+        draft.unreadChatCount = 0;
+        return;
     }
   }
 
-  const [state, dispatch] = useImmerReducer(ourReducer, initialState)
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState);
 
   useEffect(() => {
     if (state.loggedIn) {
-      localStorage.setItem("complexappToken", state.user.token)
-      localStorage.setItem("complexappUsername", state.user.username)
-      localStorage.setItem("complexappAvatar", state.user.avatar)
+      localStorage.setItem("complexappToken", state.user.token);
+      localStorage.setItem("complexappUsername", state.user.username);
+      localStorage.setItem("complexappAvatar", state.user.avatar);
     } else {
-      localStorage.removeItem("complexappToken")
-      localStorage.removeItem("complexappUsername")
-      localStorage.removeItem("complexappAvatar")
+      localStorage.removeItem("complexappToken");
+      localStorage.removeItem("complexappUsername");
+      localStorage.removeItem("complexappAvatar");
     }
-  }, [state.loggedIn])
+  }, [state.loggedIn]);
 
   return (
     <StateContext.Provider value={state}>
@@ -79,7 +96,10 @@ function Main() {
           <Header />
           <Routes>
             <Route path="/profile/:username/*" element={<Profile />} />
-            <Route path="/" element={state.loggedIn ? <Home /> : <HomeGuest />} />
+            <Route
+              path="/"
+              element={state.loggedIn ? <Home /> : <HomeGuest />}
+            />
             <Route path="/post/:id" element={<ViewSinglePost />} />
             <Route path="/post/:id/edit" element={<EditPost />} />
             <Route path="/create-post" element={<CreatePost />} />
@@ -87,19 +107,25 @@ function Main() {
             <Route path="/terms" element={<Terms />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-          <CSSTransition timeout={330} in={state.isSearchOpen} classNames="search-overlay" unmountOnExit>
+          <CSSTransition
+            timeout={330}
+            in={state.isSearchOpen}
+            classNames="search-overlay"
+            unmountOnExit
+          >
             <Search />
           </CSSTransition>
+          <Chat />
           <Footer />
         </BrowserRouter>
       </DispatchContext.Provider>
     </StateContext.Provider>
-  )
+  );
 }
 
-const root = ReactDOM.createRoot(document.querySelector("#app"))
-root.render(<Main />)
+const root = ReactDOM.createRoot(document.querySelector("#app"));
+root.render(<Main />);
 
 if (module.hot) {
-  module.hot.accept()
+  module.hot.accept();
 }
